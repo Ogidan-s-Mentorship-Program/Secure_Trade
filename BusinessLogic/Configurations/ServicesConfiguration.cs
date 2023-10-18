7afd20e48acae2362a1c19d16df44d86cb1238ed
Joshua Olorunshola <joshaadele@gmail.com>
2023-10-18 10:12:27 +0100

    Added GenericRepository Added IGenericRepository Added ServicesConfiguration

---
+++ BusinessLogic/Configurations/ServicesConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SecureTrade.DataAccess.Repository.Implementation;
+using SecureTrade.DataAccess.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureTrade.BusinessLogic.Configurations
+{
+    public static class ServicesConfiguration
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        }
+    }
+}
